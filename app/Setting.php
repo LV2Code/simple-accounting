0a0906d7e5475206188e3ec70c56450771214926
shashiOrdant <shashikant@ordant.com>
2020-04-14 22:07:37 +0530

    V1-001 added transformed method on class

--- app/Setting.php
+++ app/Setting.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     protected $guarded = [];
+
+
+    public static function transformed() {
+    	return collect(array_combine(array_column(static::all()->toArray(), 'name'), static::all()->toArray()));
+    }
 }
