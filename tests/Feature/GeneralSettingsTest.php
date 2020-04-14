45d0f3e39b876471b9477e0d36852581ae1df10d
shashiOrdant <shashikant@ordant.com>
2020-04-14 22:10:34 +0530

    V1-001 updated feature tests for general settings

--- tests/Feature/GeneralSettingsTest.php
+++ tests/Feature/GeneralSettingsTest.php
@@ -13,10 +13,7 @@ class GeneralSettingsTest extends TestCase
     public function an_authenticated_user_can_fetch_general_settings()
     {
         $this->signIn();
-
-        $settings = Setting::all();
-
-        $this->assertEquals('Company', $settings[0]['value']);
+        $this->get('/settings')->assertSee(Setting::all()[0]['value']);
     }
 
     /** @test */
@@ -54,18 +51,16 @@ class GeneralSettingsTest extends TestCase
         $this->updateSettings($settings)->assertSessionHasErrors('zip_code');
     }
 
-
     /** @test */
     public function general_settings_created_with_valid_data()
     {
-        $settingsData = raw(Setting::class);
-        $this->updateSettings($settingsData)->assertStatus(200);
+        $settings = raw(Setting::class);
 
+        $this->updateSettings($settings)
+            ->assertSessionHasNoErrors()
+            ->assertSessionHas('success');
 
-        $settings = Setting::all();
-        $this->assertEquals($settingsData['company_name'], $settings[0]['value']);
-
-        //REST
+        $this->get('/settings')->assertSee($settings['company_name']);
     }
 
 
